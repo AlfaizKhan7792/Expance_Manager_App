1cf638d6fd8216c4f30f92797b1ea6899ecac9da
AlfaizKhan7792 <alfaizkhan7792@gmail.com>
2025-04-23 16:09:10 +0530

    cors handle

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const connect_DB = require('./config/db_config');
 const MiddleWere = require('./MiddleWere/ErrorHandler');
 require("dotenv").config()
 const path = require("path")
+const cors = require("cors")
 
 
 const app = express()
@@ -17,6 +18,17 @@ app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
 
+// Cors Error Handle
+app.use(cors({
+    origin: '*', // 🌐 Public access for any frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: false // ❌ Cookies nahi use ho rahe, toh false
+  }));
+  
+  app.options('*', cors()); // Preflight ko bhi allow karo
+  app.use(express.json());
+
+
 // User ROutes
 app.use("/api/user" , require("./routes/auth/AuthRoutes"))
 
@@ -25,15 +37,15 @@ app.use("/api/trans" , require("./routes/AddTransaction/AddTransactionRoutes"))
 
 
 // Diployment Code in render
-if(process.env.NODE_ENV === "production"){
-    const __dirname = path.resolve();
-    app.use("/uploads", express.static("/var/data/uploads"));
-    app.use(express.static(path.join(__dirname, "client", "dist")));
+// if(process.env.NODE_ENV === "production"){
+//     const __dirname = path.resolve();
+//     app.use("/uploads", express.static("/var/data/uploads"));
+//     app.use(express.static(path.join(__dirname, "client", "dist")));
 
-    app.get("*", (req, res) =>
-        res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
-    );
-}
+//     app.get("*", (req, res) =>
+//         res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+//     );
+// }
     
     
 app.get("/" , (req,res) =>{
--- client/src/features/Auth/AuthService.js
+++ client/src/features/Auth/AuthService.js
@@ -1,9 +1,11 @@
 import {toast} from "react-toastify"
 import axios from "axios"
 
+const API_URL = "https://expance-manager-app.onrender.com"
+
 export const Login = async (formData) =>{
     try {
-        const res = await axios.post("/api/user/login" , formData)
+        const res = await axios.post(`${API_URL}/api/user/login` , formData)
     localStorage.setItem("user" , JSON.stringify(res.data))
     return res.data
     } catch (error) {
@@ -13,7 +15,7 @@ export const Login = async (formData) =>{
 
 export const Register = async (formData) =>{
     try {
-        const res = await axios.post("/api/user/register" , formData)
+        const res = await axios.post(`${API_URL}/api/user/register` , formData)
     localStorage.setItem("user" , JSON.stringify(res.data))
     return res.data
     } catch (error) {
